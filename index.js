28703211cab41a334f3ee7e6a5e671ccfef4246d
Honeythomas <117334674+Honeythomas@users.noreply.github.com>
2023-03-27 11:48:12 +0530

    Hoisting

---
+++ index.js
@@ -0,0 +1,32 @@
+// let promise = new Promise(function (resolve, reject) {
+//   alert("Hello");
+//   resolve(56);
+// });
+
+// console.log("hello world one");
+// setTimeout(function () {
+//   console.log("hello world two in 2 sec");
+// }, 2000);
+// console.log("My name is" + " Hello three");
+// console.log(promise);
+
+// var n = 2;
+// function square(num) {
+//   var ans = num * num;
+//   return ans;
+// }
+
+var x = 1;
+a();
+b();
+console.log(x);
+
+function a() {
+  var x = 10;
+  console.log(x);
+}
+
+function b() {
+  var x = 100;
+  console.log(x);
+}
