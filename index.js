3691118b1de0f103356187daee4d66ae3216943a
Honeythomas <117334674+Honeythomas@users.noreply.github.com>
2023-03-27 12:22:12 +0530

    this or window

--- index.js
+++ index.js
@@ -16,17 +16,24 @@
 //   return ans;
 // }
 
-var x = 1;
-a();
-b();
-console.log(x);
+// var x = 1;
+// a();
+// b();
+// console.log(x);
 
-function a() {
-  var x = 10;
-  console.log(x);
-}
+// function a() {
+//   var x = 10;
+//   console.log(x);
+// }
 
+// function b() {
+//   var x = 100;
+//   console.log(x);
+// }
+
+var a = 10;
 function b() {
-  var x = 100;
-  console.log(x);
+  var x = 10;
 }
+console.log(window.a);
+console.log(this.a);
