bd653729a80a826ca8f5ba7fdc9201a2b5898fd4
Honeythomas <117334674+Honeythomas@users.noreply.github.com>
2023-03-30 14:13:08 +0530

    Arrow function

--- index.js
+++ index.js
@@ -58,13 +58,18 @@
 
 //shadowing
 
-var a = 100;
-{
-  var a = 10;
-  let b = 20;
-  const c = 30;
-  console.log(a);
-  console.log(b);
-  console.log(c);
-}
-console.log(a);
+// var a = 100;
+// {
+//   var a = 10;
+//   let b = 20;
+//   const c = 30;
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
+// console.log(a);
+
+//arrow function
+hello = () => {
+  return "hello World";
+};
