edc262b88a0d9cb8686a272f0839d7032d511c5b
Honeythomas <117334674+Honeythomas@users.noreply.github.com>
2023-04-01 11:32:36 +0530

    higher order function

--- index.js
+++ index.js
@@ -361,3 +361,21 @@ const calculateArea = function (radius) {
   return output;
 };
 console.log(calculateArea(radius));
+
+const calculateDiameter = function (radius) {
+  const output = [];
+  for (let i = 0; i < radius.length; i++) {
+    output.push(2 * radius[i]);
+  }
+  return output;
+};
+console.log(calculateDiameter(radius));
+
+const calculateCircumference = function (radius) {
+  const output = [];
+  for (let i = 0; i < radius.length; i++) {
+    output.push(2 * Math.PI * radius[i]);
+  }
+  return output;
+};
+console.log(calculateCircumference(radius));
