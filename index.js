8032359d5a949defe10029f80a523ff3700a7b56
Honeythomas <117334674+Honeythomas@users.noreply.github.com>
2023-04-12 12:05:02 +0530

    .find

--- index.js
+++ index.js
@@ -471,33 +471,53 @@
 
 //promises
 
-const cart = ["shoes", "pants", "kurta", "tshirt"];
+// const cart = ["shoes", "pants", "kurta", "tshirt"];
 
-const promise = createOrder(cart);
+// createOrder(cart)
+//   .then(function (orderId) {
+//     console.log(orderId);
+//     return orderId;
+//   })
+//   .then(function (orderId) {
+//     return proceedToPayment(orderId);
+//   })
+//   .then(function(paymentInfo) {
+//     console.log(paymentInfo)
+//   })
+//   .catch(function (err) {
+//     console.log(err.message);
+//   });
 
-// {data: undefined}
-// {data: orderDetail}
+// function createOrder(cart) {
+//   const pr = new Promise(function (resolve, reject) {
+//     if (!validateCart(cart)) {
+//       const err = new Error("Cart is not valid");
+//       reject(err);
+//     }
+//     const orderId = "12345";
+//     if (orderId) {
+//       setTimeout(function () {
+//         resolve(orderId);
+//       }, 5000);
+//     }
+//   });
+//   return pr;
+// }
 
-promise.then(function (orderId) {
-  console.log(orderId);
-});
+// function proceedToPayment(orderId) {
+//   return new Promise(function (resolve, reject) {
+//     resolve("Payment Resolved");
+//   });
+// }
 
-function createOrder(cart) {
-  const pr = new Promise(function (resolve, reject) {
-    if (!validateCart(cart)) {
-      const err = new Error("Cart is not valid");
-      reject(err);
-    }
-    const orderId = "12345";
-    if (orderId) {
-      setTimeout(function () {
-        resolve(orderId);
-      }, 5000);
-    }
-  });
-  return pr;
-}
+// function validateCart(cart) {
+//   return false;
+// }
 
-function validateCart(cart) {
-  return true;
-}
+//array.find
+
+const num = [45, 55, 30, 100];
+
+const numGreaterThanFifty = num.find((element) => element > 50);
+console.log(numGreaterThanFifty);
+console.log(numGreaterThanFifty);
